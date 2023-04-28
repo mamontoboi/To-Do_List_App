e786e449bc1b70764270e414c4d51ec566b90ad8
Mamontoboi <mamontoboi@gmail.com>
2023-04-28 18:09:52 +0200

    Google Client ID and Secret were added to config

--- app/config.py
+++ app/config.py
@@ -4,6 +4,8 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     DB_URL: str
     DB_NAME: str
+    GOOGLE_CLIENT_ID: str
+    GOOGLE_CLIENT_SECRET: str
 
     class Config:
         env_file = '.env'
